dc6b36349adf7ee5fd38270489bb53e581e8b986
EGOBenCoco <kirillkasanov291@gmail.com>
2026-06-19 16:33:50 +0300

    Initial commit

---
+++ src/main/java/org/example/spring_test_ci/SpringTestCiApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_test_ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestCiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spring_test_ci/SpringTestCiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_test_ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestCiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
